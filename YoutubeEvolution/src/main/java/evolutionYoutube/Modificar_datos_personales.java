0bb24aa977d951075b2d3c956ae1b974a665c88b
Juan José Camacho <jch004@inlumine.ual.es>
2018-08-29 22:17:16 +0200

    Videos subidos parte 1

--- YoutubeEvolution/src/main/java/evolutionYoutube/Modificar_datos_personales.java
+++ YoutubeEvolution/src/main/java/evolutionYoutube/Modificar_datos_personales.java
@@ -5,6 +5,8 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
 
+import database.BD_general;
+
 public class Modificar_datos_personales extends Modificar_datos_personales_ventana implements View {
 	public Datos_personales _unnamed_Datos_personales_;
 
@@ -13,6 +15,7 @@ public class Modificar_datos_personales extends Modificar_datos_personales_venta
 
 			@Override
 			public void buttonClick(ClickEvent event) {
+				guardar();
 				((MyUI) UI.getCurrent()).mi_perfil_registrado();
 				
 			}
@@ -27,9 +30,17 @@ public class Modificar_datos_personales extends Modificar_datos_personales_venta
 			}
 			
 		});
+	    nombre.setValue(MyUI.getUsuarioLogged().getNombre());
+	    apellido.setValue(MyUI.getUsuarioLogged().getApellido());
+	    apodo.setValue(MyUI.getUsuarioLogged().getApodo());
+	    email.setValue(MyUI.getUsuarioLogged().getEmail());
+	    contrasenia.setValue(MyUI.getUsuarioLogged().getContrasenia());
+	    repetircontrasenia.setValue(MyUI.getUsuarioLogged().getContrasenia());
+	    anio.setValue(MyUI.getUsuarioLogged().getAnio());
 	}
 	public void guardar() {
-		throw new UnsupportedOperationException();
+		BD_general bd = new BD_general();
+		bd.modificar_datos(MyUI.getUsuarioLogged().getID(), nombre.getValue(), apellido.getValue(), apodo.getValue(),anio.getValue(), email.getValue(), contrasenia.getValue());
 	}
 
 	public void cancelar() {
